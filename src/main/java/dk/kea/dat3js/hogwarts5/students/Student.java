4d22bf1ea89358c76e9f1bc1e2b572004797a547
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-04-22 10:33:50 +0200

    Added fullName method to student + test

--- src/main/java/dk/kea/dat3js/hogwarts5/students/Student.java
+++ src/main/java/dk/kea/dat3js/hogwarts5/students/Student.java
@@ -3,94 +3,135 @@ package dk.kea.dat3js.hogwarts5.students;
 import dk.kea.dat3js.hogwarts5.house.House;
 import jakarta.persistence.*;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 @Entity
 public class Student {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
-  private String firstName;
-  private String middleName;
-  private String lastName;
-  @ManyToOne
-  private House house;
-  private Integer schoolYear; // 1-7
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    @ManyToOne
+    private House house;
+    private Integer schoolYear; // 1-7
 
-  public Student() {
-  }
+    public Student() {
+    }
 
-  public Student(String firstName, String lastName, House house, int schoolYear) {
-    this(firstName, null, lastName, house, schoolYear);
-  }
+    public Student(String firstName, String lastName, House house, int schoolYear) {
+        this(firstName, null, lastName, house, schoolYear);
+    }
 
-  public Student(String firstName, String middleName, String lastName, House house, int schoolYear) {
-    this.firstName = firstName;
-    this.middleName = middleName;
-    this.lastName = lastName;
-    this.house = house;
-    this.schoolYear = schoolYear;
-  }
+    public Student(String firstName, String middleName, String lastName, House house, int schoolYear) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.house = house;
+        this.schoolYear = schoolYear;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public String getFirstName() {
-    return firstName;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-  public String getMiddleName() {
-    return middleName;
-  }
+    public String getMiddleName() {
+        return middleName;
+    }
 
-  public void setMiddleName(String middleName) {
-    this.middleName = middleName;
-  }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
 
-  public String getLastName() {
-    return lastName;
-  }
+    public String getLastName() {
+        return lastName;
+    }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-  public House getHouse() {
-    return house;
-  }
+    public House getHouse() {
+        return house;
+    }
 
-  public void setHouse(House house) {
-    this.house = house;
-  }
+    public void setHouse(House house) {
+        this.house = house;
+    }
 
-  public Integer getSchoolYear() {
-    return schoolYear;
-  }
+    public Integer getSchoolYear() {
+        return schoolYear;
+    }
 
-  public void setSchoolYear(Integer schoolYear) {
-    this.schoolYear = schoolYear;
-  }
+    public void setSchoolYear(Integer schoolYear) {
+        this.schoolYear = schoolYear;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Student student = (Student) o;
-    return Objects.equals(getFirstName(), student.getFirstName()) && Objects.equals(getMiddleName(), student.getMiddleName()) && Objects.equals(getLastName(), student.getLastName()) && Objects.equals(getHouse().getName(), student.getHouse().getName());
-  }
+    public String getFullName() {
+        return firstName + " " + (middleName != null && !middleName.isEmpty() ? middleName + " " : "") + lastName;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(getFirstName(), getMiddleName(), getLastName(), getHouse().getName());
-  }
+    public String setFullName(String fullName) {
+        if (fullName == null || fullName.isEmpty()) {
+            System.out.println("Name cannot be empty or null");
+            return "Name cannot be empty or null";
+        }
+        String[] names = fullName.split(" ");
+        // remove empty strings
+        names = Arrays.stream(names).filter(s -> !s.isEmpty()).toArray(String[]::new);
+        if (names.length < 1) {
+            System.out.println("Name must contain at least 1 part");
+            return "Name must contain at least 1 part";
+        }
 
+        firstName = capitalize( names[0]);
+        if (names.length > 2) {
+            // middleName = names[1];
+            StringBuilder correctMiddle = new StringBuilder();
+            for (int i = 1; i < names.length-1; i++) {
+                correctMiddle.append(capitalize(names[i])).append(" ");
+            }
+            middleName = correctMiddle.toString().trim();
+            lastName = capitalize(names[names.length - 1]) ;
+        } else if (names.length > 1) {
+            middleName = null;
+            lastName = capitalize(names[1]) ;
+        } else {
+            middleName = null;
+            lastName = null;
+        }
+        System.out.println(getMiddleName());
+        return getFullName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(getFirstName(), student.getFirstName()) && Objects.equals(getMiddleName(), student.getMiddleName()) && Objects.equals(getLastName(), student.getLastName()) && Objects.equals(getHouse().getName(), student.getHouse().getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getMiddleName(), getLastName(), getHouse().getName());
+    }
+
+    private String capitalize(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
 }
---
+++ src/test/java/dk/kea/dat3js/hogwarts5/students/StudentTest.java
@@ -0,0 +1,196 @@
+package dk.kea.dat3js.hogwarts5.students;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getFullNameWithMiddleName() {
+        //arrange
+        Student student = new Student("Harry", "James", "Potter", null, 1);
+        String expected = "Harry James Potter";
+
+        //act
+        String actual = student.getFullName();
+
+        //assert
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void getFullNameWithNullMiddleName() {
+        //arrange
+        Student student = new Student("Harry", null, "Potter", null, 1);
+        String expected = "Harry Potter";
+
+        //act
+        String actual = student.getFullName();
+
+        //assert
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void getFullNameWithEmptyMiddleName() {
+        //arrange
+        Student student = new Student("Harry", "", "Potter", null, 1);
+        String expected = "Harry Potter";
+
+        //act
+        String actual = student.getFullName();
+
+        //assert
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void setFullNameWithSingleMiddleName() {
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "Harry James Potter";
+
+        //act
+        student.setFullName("Harry James Potter");
+
+
+        //assert
+        assertEquals("Harry", student.getFirstName());
+        assertEquals("James", student.getMiddleName());
+        assertEquals("Potter", student.getLastName());
+
+    }
+
+    @Test
+    void setFullNameWithNullMiddleName() {
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "Harry Potter";
+
+        //act
+        student.setFullName("Harry Potter");
+
+
+        //assert
+        assertEquals("Harry", student.getFirstName());
+        assertNull(student.getMiddleName());
+        assertEquals("Potter", student.getLastName());
+
+    }
+
+    @Test
+    void setFullNameWithoutLastName() {
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "Harry";
+
+        //act
+        student.setFullName("Harry");
+
+
+        //assert
+        assertEquals("Harry", student.getFirstName());
+        assertNull(student.getMiddleName());
+        assertNull(student.getLastName());
+
+    }
+
+    @Test
+    void setFullNameWithMultipleMiddleNames() {
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+
+        //act
+        student.setFullName("Albus Percival Wulfric Brian Dumbledore");
+
+        //assert
+        assertEquals("Albus", student.getFirstName());
+        assertEquals("Percival Wulfric Brian", student.getMiddleName());
+        assertEquals("Dumbledore", student.getLastName());
+    }
+
+    @Test
+    void setFullNameWithSpaceAsMiddleName() {
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+
+        //act
+        student.setFullName("Albus     Dumbledore");
+
+        //assert
+        assertEquals("Albus", student.getFirstName());
+        assertNull(student.getMiddleName());
+        assertEquals("Dumbledore", student.getLastName());
+    }
+    @Test
+    void setFullnameWithEmptyString(){
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "first middle last";
+
+        //act
+        student.setFullName("");
+
+        //assert
+        assertEquals("first middle last", student.getFullName());
+    }
+
+    @Test
+    void setFullNameWithMultipleEmptyStrings(){
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "first middle last";
+
+        //act
+        student.setFullName("   ");
+
+        //assert
+        assertEquals("first middle last", student.getFullName());
+    }
+
+    @Test
+    void setFullnameWithNull(){
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "first middle last";
+
+        //act
+        student.setFullName(null);
+
+        //assert
+        assertEquals("first middle last", student.getFullName());
+    }
+
+    @Test
+    void capitalizeIndividualNames(){
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "First Middle Last";
+
+        //act
+        student.setFullName("first middle1 middle2 middle3 last");
+
+        //assert
+        assertEquals("First", student.getFirstName());
+        assertEquals("Middle1 Middle2 Middle3", student.getMiddleName());
+        assertEquals("Last", student.getLastName());
+    }
+
+    @Test
+    void capitalizeIndividualNamesWithCrazyCharacters(){
+        //arrange
+        Student student = new Student("first", "middle", "last", null, 1);
+        String expected = "First Middle Last";
+
+        //act
+        student.setFullName("fIrSt mIdDlE lAsT");
+
+        //assert
+        assertEquals(expected, student.getFullName());
+    }
+
+}
